86c8bb8769c1971edf644faca33b88b90f0d92dc
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-12-08 14:05:18 -0600

    feat: refactor calendar integration to use derived state for selected date in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,21 @@
 	import { Calendar } from '$lib/components/ui/calendar/index.js';
 	import { Calendar as EventCalendar, TimeGrid } from '@event-calendar/core';
 
-	let options = {
+    let selectedDate = $state(today(getLocalTimeZone()));
+	let options = $derived({
 		view: 'timeGridWeek',
 		events: [
 			// { id, title, start, end, ... }
-		]
-	};
-	let value = today(getLocalTimeZone());
+		],
+        date: selectedDate.toDate(getLocalTimeZone()),
+	});
+
 </script>
 
 <div class="flex flex-row gap-4">
-	<Calendar type="single" bind:value class="rounded-md border" />
-    <div class="w-1"></div>
-    <div class="flex-1">
+	<Calendar type="single" bind:value={selectedDate} class="rounded-md border" />
+	<div class="w-1"></div>
+	<div class="flex-1">
 		<EventCalendar plugins={[TimeGrid]} {options} />
 	</div>
 </div>
